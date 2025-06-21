6d2d0ba6cb95d8b93d3c2948ee5a72b60f195bf7
1caicai1 <913319466@qq.com>
2025-06-21 11:22:53 +0800

    Delete src directory

--- src/terrain/mesh/block.js
+++
@@ -1,9 +0,0 @@
-export default class Block {
-  constructor(x, y, z, type, placed) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this.type = type;
-    this.placed = placed;
-  }
-}
--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-export const htmlToDom = (html) => {
-  const templateDom = document.createElement('template');
-  templateDom.innerHTML = html;
-  document.body.appendChild(templateDom.content);
-};
-
-export const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
